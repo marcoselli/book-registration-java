031b798bbd21fa4090e7499e6c7244bc9e1fadde
Marco <marco_sellI@@hotmail.com>
2021-11-26 11:22:02 -0300

    first commit

---
+++ src/main/java/br/com/cadastro/livros/CadastroDeLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastro.livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cadastro/livros/CadastroDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadastro.livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
